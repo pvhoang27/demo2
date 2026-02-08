1f64a7eb1b0fd79e89254f4c8ffaaf36cc119a78
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 11:00:20 +0700

    3

--- src/lessons/Lesson3/Lesson3.jsx
+++ src/lessons/Lesson3/Lesson3.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const Lesson3 = (props) => {
-    const [name , setName] = useState('');
-    return (
-        <div>
-            <label>Name:</label>
-            <input type={'text'} 
-            onChange={(event) => setName(event.target.value)}
-            value={name}
-            />
-            <div>Your name 's ...{name} </div>
-        </div>
-    )
-}
+  const [name, setName] = useState("");
+  return (
+    <div>
+      <label>Name:</label>
+      <input
+        type={"text"}
+        onChange={(event) => setName(event.target.value)}
+        value={name}
+      />
+      <div>Your name 's ...{name} </div>
+    </div>
+  );
+};
 
 export default Lesson3;
