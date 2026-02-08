2ba4c06fe139acbf9602a6148822a0f06ee25782
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 10:56:15 +0700

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css';
-import NavBar from './components/Header/NavBar';
-import Content from './components/Main/Content';
-import Footer from './components/Footer/Footer';
-
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/Header/NavBar";
+import Content from "./components/Main/Content";
+import Footer from "./components/Footer/Footer";
+import Lesson3 from "./components/Lessons/Lesson3";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -11,13 +11,15 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <Content
+      {/* <Content
         count={count}
         setCount={setCount}
-      />
+      /> */}
+
+      <Lesson3 />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
