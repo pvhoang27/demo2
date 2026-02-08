d048d714b66e9a47bd7cef12900265f62e0151cf
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 18:49:41 +0700

    7

--- src/lessons/Lesson7/Lesson7.jsx
+++ src/lessons/Lesson7/Lesson7.jsx
@@ -1,5 +1,8 @@
 const Lesson7 = (props) => {
     
+    function (){
+        console.log("Anonymous function called");
+    }
     return (
         <div style={{ padding: "50px" }}>
             <h3>Lesson 7 Anonymous Function:</h3>
