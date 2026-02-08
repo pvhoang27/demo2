c5e858ea91239ca4704e8fd04505ac8db06f4c67
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 19:07:23 +0700

    9

--- src/lessons/Lesson9/Lesson9.jsx
+++ src/lessons/Lesson9/Lesson9.jsx
@@ -1,14 +1,14 @@
 import EventPreventDefault from "./EventPreventDefault";
 import EventPropagation from "./EventPropagation";
-import './Lesson9.scss';
+import "./Lesson9.scss";
 
 const Lesson9 = (props) => {
-    return (
-        <div className="lesson9-container">
-            <EventPreventDefault />
-            {/* <EventPropagation /> */}
-        </div>
-    )
-}
+  return (
+    <div className="lesson9-container">
+      <EventPreventDefault />
+      {/* <EventPropagation /> */}
+    </div>
+  );
+};
 
 export default Lesson9;
