0d3304c36d1930e8b426d26e499257c526efbc55
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-09 10:10:27 +0700

    18

--- src/project/chapter3/Lesson18.jsx
+++ src/project/chapter3/Lesson18.jsx
@@ -3,17 +3,17 @@ const Lesson18 = () => {
     let inProgress = true;
     let completed = false;
 
-    console.log(typeof completed); // boolean
+    // console.log(typeof completed); // boolean
 
-    console.log(Boolean('Hi'));// true
-    console.log(Boolean(''));  // false
+    // console.log(Boolean('Hi'));// true
+    // console.log(Boolean(''));  // false
 
-    console.log(Boolean(20));  // true
-    console.log(Boolean(Infinity));  // true
-    console.log(Boolean(0));  // false
+    // console.log(Boolean(20));  // true
+    // console.log(Boolean(Infinity));  // true
+    // console.log(Boolean(0));  // false
 
-    console.log(Boolean({ foo: 100 }));  // true on non-empty object
-    console.log(Boolean(null));// false
+    // console.log(Boolean({ foo: 100 }));  // true on non-empty object
+    // console.log(Boolean(null));// false
 
     // let name = 'eric'; //datatype: string
     // if(name){ //do something}
