f082b2043a1d8d5cdeb84ef5bd9e2fccd6e3c1d3
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-09 09:14:52 +0700

    10

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Lesson6 from "./lessons/Lesson6/Lesson6";
 import Lesson7 from "./lessons/Lesson7/Lesson7";
 import Lesson8 from "./lessons/Lesson8/Lesson8";
 import Lesson9 from "./lessons/Lesson9/Lesson9";
+import Lesson10 from "./lessons/Lesson10/Lesson10";
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -26,7 +27,8 @@ const App = () => {
       {/* <Lesson6 /> */}
       {/* <Lesson7 /> */}
       {/* <Lesson8 /> */}
-      <Lesson9 />
+      {/* <Lesson9 /> */}
+      <Lesson10 />
       <Footer />
     </div>
   );
