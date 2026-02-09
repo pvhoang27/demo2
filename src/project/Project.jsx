f5ac5d3f0391bd2ffd1ae55c6395d0852beb82d7
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-09 11:21:21 +0700

    33

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -3,12 +3,14 @@ import "./Project.scss";
 import "nprogress/nprogress.css";
 // import Lesson26 from "./chapter4/lesson26/Lesson26";
 // import Lesson27 from "./chapter4/lesson27/Lesson27";
-import Lesson29 from "./chapter4/lesson29/Lesson29";
+// import Lesson29 from "./chapter4/lesson29/Lesson29";
+// import Lesson32 from "./chapter4/lesson32/Lesson32";
+import Lesson33 from "./chapter4/lesson33/Lesson33";
 const Project = (props) => {
   return (
     <div className="project">
       <Login />
-      <Lesson29 />
+      <Lesson33 />
     </div>
   );
 };
