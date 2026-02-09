dd7066fde785a00d64c0b86cbfb5534aebaab076
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-09 09:25:53 +0700

    11

--- src/lessons/Lesson10/Lesson10.jsx
+++ src/lessons/Lesson10/Lesson10.jsx
@@ -1,49 +1,50 @@
-import CalculateVAT from './CalculateVAT';
-import './Lesson10.scss';
+import CalculateVAT from "./CalculateVAT";
+import "./Lesson10.scss";
+import { useEffect, useRef } from "react";
 
+const firstRenderRef = useRef(true);
 
+useEffect(() => {
+  if (firstRenderRef.current && firstRenderRef === true) {
+    firstRenderRef.current = false;
+  }
+}, []);
 
 const Lesson10 = (props) => {
-    //  pure function
-    function calculateVAT(productPrice) {
-        return productPrice * 0.05;
-    }
+  //  pure function
+  function calculateVAT(productPrice) {
+    return productPrice * 0.05;
+  }
+  if (firstRenderRef.current === true) {
+    console.log(">>> VAT === ", calculateVAT(100));
+  }
 
-    console.log(">>> VAT === ", calculateVAT(100))
+  // ex not pure (impure) function (không phụ thuộc vào input đầu vào)
+  var tax = 5;
+  function calculateVAT1(productPrice) {
+    return (productPrice * tax) / 100;
+  }
 
+  function doSomething() {
+    //..whatever
+    tax = 80;
+  }
+  doSomething();
+  console.log(">>> VAT impure === ", calculateVAT1(100));
 
-    // ex not pure (impure) function (không phụ thuộc vào input đầu vào)
-    var tax = 5;
-    function calculateVAT1(productPrice) {
-        return productPrice * tax / 100;
-    }
-
-    function doSomething() {
-        //..whatever
-        tax = 80;
-    }
-    doSomething();
-    console.log(">>> VAT impure === ", calculateVAT1(100))
-
-
-
-    return (
-        <div className='lesson10-container'>
-            Lesson 10 : pure-component
-
-            {/* impure component */}
-            {/* <CalculateVAT />
+  return (
+    <div className="lesson10-container">
+      Lesson 10 : pure-component
+      {/* impure component */}
+      {/* <CalculateVAT />
             <CalculateVAT />
             <CalculateVAT /> */}
-
-            {/* pure component */}
-            {/* <CalculateVAT tax={5} price={100} />
+      {/* pure component */}
+      {/* <CalculateVAT tax={5} price={100} />
             <CalculateVAT tax={5} price={100} />
             <CalculateVAT tax={5} price={100} /> */}
-
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Lesson10;
